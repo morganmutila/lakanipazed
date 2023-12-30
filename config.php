e3ee6c5634d72c63fc7f793a231620fbd65975a1
Morgan Mutila <mutilamorgan@gmail.com>
2023-12-30 17:13:10 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "lakanipazed_db";
+
+// Site URL
+$site_url = "http://localhost/lakanipazed"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "htrtjykybktktktykktktyktyktyk"; // Your purchase code, don't give it to anyone.
+?>
---
+++ themes/soundify/info.php
@@ -0,0 +1,5 @@
+<?php
+$themeName = 'soundify';
+$themeVirsion = '1.5.2';
+$themeAuthor = 'Kulvir Singh';
+$themeAuthorUrl = 'http://bit.ly/kb97';
